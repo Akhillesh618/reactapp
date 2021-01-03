722ae66189f28284b8000b4b0fc8407589cf98cc
Akhilesh <akhivvvv@gmail.com>
2021-01-03 18:03:09 +0530

    inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import react from 'react-dom'
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+       <div className="App">
+       <div id="root"></div>
+         <h1>Random Images</h1> 
+       </div>
+
+        <div className="row" >
+         <img className = "App-logo" src="https://source.unsplash.com/random/550x700?sig=3"  alt=""/>
+         <img className = "App-logo" src="https://source.unsplash.com/random/550x700?sig=1"  alt=""/>
+         <img className = "App-logo" src="https://source.unsplash.com/random/550x700?sig=2/" alt=""/>
+        </div>
+     
       </header>
     </div>
+    
   );
+
+
+
+  function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    react.render(element, document.getElementById('root'));
+  }
+  
+  setInterval(tick, 1000);
+
+
+  
 }
+
+
 
 export default App;
